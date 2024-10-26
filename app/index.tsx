993018b26a52feb7bb9cd11b983214e4dab83a6b
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-10-26 14:51:43 +0800

    Refactor app index component and styles

---
+++ app/index.tsx
@@ -0,0 +1,30 @@
+import { Text, View, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello Expo.</Text>
+      <Link href={'/about'} style={styles.button}>
+        Go to About Screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#25292e',
+  },
+  text: {
+    color: 'white',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: 'blue',
+  },
+});
