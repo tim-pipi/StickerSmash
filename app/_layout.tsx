b59de07440f07d00c83a4271420016cecf059c70
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-10-26 15:25:33 +0800

    feat: add StatusBar style to light - to match with the theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,24 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="+not-found"
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <>
+      <StatusBar style="light" />
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="+not-found"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </>
   );
 }
