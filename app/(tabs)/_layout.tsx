ac4d9e8d0cba1f93d3a9f6d241da0f6d3ce0ca16
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-10-26 15:19:21 +0800

    feat: add home icon to tabs layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,24 @@
 import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabsLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "#ffd33d"
+        tabBarActiveTintColor: '#ffd33d',
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
           headerTitle: 'Sticker Smash',
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? 'home-sharp' : 'home-outline'}
+              color={color}
+              size={30}
+            />
+          ),
         }}
       />
       <Tabs.Screen
