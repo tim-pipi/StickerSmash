e4e78c281abdacd2b5982fe8629b3c9f649bea9f
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-10-26 14:57:25 +0800

    refactor: update entrypoint to (tabs)
    previously, it was using the normal stack layout to index

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen
-        name="index"
+        name="(tabs)"
         options={{
           headerTitle: 'Sticker Smash',
           headerLeft: () => <></>,
