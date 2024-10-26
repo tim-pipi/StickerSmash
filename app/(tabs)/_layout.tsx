f70cdee9247851d598d92ca2b17702728303dd9e
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-10-26 15:11:00 +0800

    feat: update bottom Tab appearance to yellow

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,12 +2,15 @@ import { Tabs } from 'expo-router';
 
 export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#ffd33d"
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           headerTitle: 'Sticker Smash',
-          headerLeft: () => <></>,
         }}
       />
       <Tabs.Screen
