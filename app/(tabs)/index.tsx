a1dac2ac2254cef75d44397f7e78141b292bdc9a
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-10-26 15:07:26 +0800

    refactor: remove unused Link component from tabs layout
    Users can access the about page through the tab instead

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,9 @@
 import { Text, View, StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello Expo.</Text>
-      <Link href={'/about'} style={styles.button}>
-        Go to About Screen
-      </Link>
     </View>
   );
 }
