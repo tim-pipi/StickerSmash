0903b6bf7139173969994f8c711aa22741772689
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-11-14 15:24:29 +0800

    feat: Add conditional state to check if user has selected an image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,12 @@ import { useState } from 'react';
 const PlaceholderImage = require('../../assets/images/background-image.png');
 
 export default function Index() {
-  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined
+  );
+
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
@@ -26,11 +31,21 @@ export default function Index() {
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={selectedImage || PlaceholderImage} />
       </View>
-
-      <View style={styles.footerContainer}>
-        <Button theme='primary' label='Choose a photo' onPress={pickImageAsync} />
-        <Button label='Use this photo' />
-      </View>
+      {showAppOptions ? (
+        <View />
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            theme='primary'
+            label='Choose a photo'
+            onPress={pickImageAsync}
+          />
+          <Button
+            label='Use this photo'
+            onPress={() => setShowAppOptions(true)}
+          />
+        </View>
+      )}
     </View>
   );
 }
