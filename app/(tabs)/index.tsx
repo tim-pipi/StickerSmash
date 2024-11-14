c15df855db39659e198ba954635eca7a56dd5bb5
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-11-14 14:57:16 +0800

    feat: Add 2 Buttons to index and style the container

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
-import { View, StyleSheet } from 'react-native';  
+import { View, StyleSheet } from 'react-native';
 import ImageViewer from '@/components/ImageViewer';
+import Button from '@/components/Button';
 
 const PlaceholderImage = require('../../assets/images/background-image.png');
 
@@ -8,6 +9,11 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Button label='Choose a photo' />
+        <Button label='Use this photo' />
       </View>
     </View>
   );
@@ -28,5 +34,10 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingHorizontal: 50,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
