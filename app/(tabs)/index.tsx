aad6e01ac47c42081deba1d81ce45c7c76dd16d9
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-11-14 15:08:30 +0800

    feat: Add pickImageAsync function to button's on press

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,24 @@
 import { View, StyleSheet } from 'react-native';
 import ImageViewer from '@/components/ImageViewer';
 import Button from '@/components/Button';
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceholderImage = require('../../assets/images/background-image.png');
 
 export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
@@ -12,7 +26,7 @@ export default function Index() {
       </View>
 
       <View style={styles.footerContainer}>
-        <Button theme='primary' label='Choose a photo' />
+        <Button theme='primary' label='Choose a photo' onPress={pickImageAsync} />
         <Button label='Use this photo' />
       </View>
     </View>
